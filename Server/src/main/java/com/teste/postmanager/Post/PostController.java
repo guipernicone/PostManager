a5491b410b710b38b3011457ed92824d76e09501
guipernicone <guipernicone@hotmail.com>
2020-12-03 06:23:17 -0300

    Adiciona suporte para cross origin

--- Server/src/main/java/com/teste/postmanager/Post/PostController.java
+++ Server/src/main/java/com/teste/postmanager/Post/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/post")
+@CrossOrigin
 public class PostController {
 
 	@Autowired
